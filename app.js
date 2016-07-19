50d89a73fbfe691c57ec2d9cc41f96a8ff1e89ce
Gary Leutheuser <g.leutheuser@gmail.com>
2016-07-19 18:03:26 +0000

    Add basic functionality.
    Retrieves user's IP address, language, and operating system details.

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+
+
+app.get('/', function(req, res) {
+    var softwareRegex = /\(([^\)]+)\)/;
+    
+    var software = req.headers['user-agent'].match(softwareRegex)
+    
+    console.log(software);
+    
+    var resObj = {
+        'ipaddress': req.headers['x-forwarded-for'],
+        'language': req.headers['accept-language'].substring(0, 5),
+        'software': software[0].substring(1, software[0].length - 1)
+    }
+    
+    res.end(JSON.stringify(resObj));
+});
+
+app.listen(process.env.PORT || 5000);
